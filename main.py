159909b9e6c79134e3c43223d16ede8d07f74ffc
tententgc <thanyapisit.bua@gmail.com>
2025-02-10 21:41:43 +0700

    fix the problem with longtitude and latitude

--- main.py
+++ main.py
@@ -2,35 +2,39 @@ from flask import Flask, request, jsonify
 from pop_score import get_poptotal_by_dname
 from detection_image import call_model
 
-# Initialize Flask application
+
 app = Flask(__name__)
 
 @app.route('/process-image', methods=['POST'])
 def process_image():
     try:
-        # Get JSON payload from request
         data = request.get_json()
 
-        # Extract required fields
+        
         image_path = data.get('image_path')
         model_name = data.get('model_name')
         city_name = data.get('city_name')
+        latitude = data.get('latitude')
+        longtitude = data.get('longtitude')
 
-        # Validate request payload
+        
+        # not adding the latitude and longtitude to must required
         if not all([image_path, model_name, city_name]):
             return jsonify({"error": "Missing required fields: 'image_path', 'model_name', 'city_name'"}), 400
 
-        # Get population score
+       
         pop_score = get_poptotal_by_dname(city_name)
 
-        # Get model detection results
+        
         damage_score, class_detected = call_model(model_name, image_path)
 
-        # Return response
+        
         return jsonify({
             "class_detected": class_detected,
             "damage_score": damage_score,
-            "pop_score": pop_score
+            "pop_score": pop_score,
+            "latitude" : latitude, 
+            "longtitude": longtitude
         })
 
     except Exception as e:
